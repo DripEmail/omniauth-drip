8f8b38e87f9ef758d639fcf25c2ff2176a852c5b
Ryan Heneise <ryan@mysmallidea.com>
2017-03-29 15:21:54 -0500

    Fixed `OAuth2::Error, invalid_grant` with OmniAuth::Oauth2 v1.4.0

--- lib/omniauth/strategies/drip.rb
+++ lib/omniauth/strategies/drip.rb
@@ -38,6 +38,10 @@ module OmniAuth
       def raw_info
         @raw_info ||= JSON.parse(access_token.get("/v2/accounts").body)
       end
+
+      def callback_url
+        options[:redirect_uri] || (full_host + script_name + callback_path)
+      end
     end
   end
 end
